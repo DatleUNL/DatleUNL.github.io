a663488d33a7c2595204d3af6881e692071b9337
Dat Le <69866173+DatleUNL@users.noreply.github.com>
2023-03-27 03:00:12 -0500

    added the ability to toggle dark/light mode

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,5 +1,17 @@
-function toggleMode() {
-    document.body.classList.toggle("light-mode");
+var mode = localStorage.getItem('mode') || 'dark';
+
+setMode(mode);
+
+function toggleDarkMode() { 
+    mode = mode === 'light' ? 'dark' : 'light';
+
+    localStorage.setItem('mode', mode);
+
+    setMode(mode);
 }
 
-document.getElementById("mode-toggle").addEventListener("click", toggleMode);
+function setMode(mode) { 
+    var body = document.body;
+    body.classList.remove('light-mode', 'dark-mode');
+    body.classList.add(mode + '-mode');
+}
